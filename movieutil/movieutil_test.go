e11ba7f209ef077d808db7b058db4b922a857f6b
Diego Bezerra <dsouzaduarte@gmail.com>
2019-02-24 02:43:55 -0300

    Add more tests

--- movieutil/movieutil_test.go
+++ movieutil/movieutil_test.go
@@ -3,6 +3,7 @@ package movieutil
 import (
 	"net/url"
 	"testing"
+	"time"
 )
 
 func TestIDFromURL(t *testing.T) {
@@ -19,5 +20,55 @@ func TestIDFromURL(t *testing.T) {
 	ID, _ = IDFromURL(u2)
 	if expected != ID {
 		t.Fatalf("expected %d, got %d", expected, ID)
+	}
+}
+
+func TestSlugFromURL(t *testing.T) {
+	_, err := SlugFromURLString("invalid url")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	u := "http://www.claquete.com/7785/aquaman.html"
+
+	expected := "aquaman"
+	actual, err := SlugFromURLString(u)
+	if err != nil {
+		t.Fatalf("expected no error, but got error: %s", err.Error())
+	}
+
+	if expected != actual {
+		t.Fatalf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestParseRuntime(t *testing.T) {
+	actual, err := ParseRuntime("90")
+	if err != nil {
+		t.Fatalf("expected no error, but got error: %s", err.Error())
+	}
+
+	expected := 90
+	if actual != expected {
+		t.Fatalf("expected %d, got %d", expected, actual)
+	}
+
+	_, err = ParseRuntime("definitely not a runtime")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+}
+
+func TestParseReleaseDate(t *testing.T) {
+	actual, err := ParseReleaseDate("07/03/2019", "/")
+	if err != nil {
+		t.Fatalf("expected no error, but got error: %s", err.Error())
+	}
+
+	expected := time.Date(2019, time.March, 7, 0, 0, 0, 0, time.UTC)
+
+	eYear, eMonth, eDay := expected.Date()
+	if eYear != actual.Year() || eMonth != actual.Month() || eDay != actual.Day() {
+		t.Fatalf("expected %s, but got %s", expected, actual)
 	}
 }
